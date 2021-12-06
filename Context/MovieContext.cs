94fdd2de3f0b31b42906a1b8044dbb423814ec82
lynnsimonen <79551770+lynnsimonen@users.noreply.github.com>
2021-12-05 23:14:16 -0600

    Update MovieContext.cs

--- Context/MovieContext.cs
+++ Context/MovieContext.cs
@@ -12,10 +12,6 @@ namespace MovieLibrary_DB1.Context
         public DbSet<Occupation> Occupations {get;set;}
         public DbSet<User> Users {get;set;}
         public DbSet<UserMovie> UserMovies {get;set;}
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=bitsql.wctc.edu;Database=lsimonen_12090_Movie;User ID=lsimonen;Password=000259304;");
-        }       
+  
     }
 }
