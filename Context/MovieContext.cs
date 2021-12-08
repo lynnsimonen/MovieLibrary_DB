888cf6ae1b8f4b6a9bac1fde1a4d8fbf53b56bcd
Lynn Simonen <lynnsimonen@yahoo.com>
2021-12-08 08:42:19 -0600

    update

--- Context/MovieContext.cs
+++ Context/MovieContext.cs
@@ -13,9 +13,5 @@ namespace MovieLibrary_DB1.Context
         public DbSet<User> Users {get;set;}
         public DbSet<UserMovie> UserMovies {get;set;}
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=bitsql.wctc.edu;Database=lsimonen_12090_Movie;User ID=lsimonen;Password=000259304;");
-        }
     }
 }
